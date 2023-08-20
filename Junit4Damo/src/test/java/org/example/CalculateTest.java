530dc95394cadbaefaeb2d78b7bc56774e71cb15
yuanguang <xinyuedeguang@gmail.com>
2023-08-20 21:26:42 +0800

    上传原有项目文件

---
+++ Junit4Damo/src/test/java/org/example/CalculateTest.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+
+    @Test
+    public void add() {
+        assertEquals(5,new Calculate().add(3,3));
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void multiply() {
+    }
+
+    @Test
+    public void divide() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testMultiply() {
+    }
+
+    @Test
+    public void testDivide() {
+    }
+}
